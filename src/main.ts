971cf88622ddcbe5e9117fb3d0e4ae91aabb8d3d
MohsenShahbazi <mohsenshahbaze@gmail.com>
2023-06-05 17:12:06 +0330

    add Sentry config and add some style to project add multiligule files to project add ngx cookie and ngx toastr to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,40 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import * as Sentry from "@sentry/angular-ivy";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+
+Sentry.init({
+  dsn: "https://136c55e3c691465d968a52ab0973cff9@o1140377.ingest.sentry.io/6197517",
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+    // Registers the Replay integration,
+    // which automatically captures Session Replays
+    new Sentry.Replay(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
